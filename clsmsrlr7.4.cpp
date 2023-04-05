2a70f322a5c09d5bfa8a475a0142af061405a0da
PYTHON48484840101101100212 <112691646+PYTHON48484840101101100212@users.noreply.github.com>
2023-04-05 19:23:51 +0300

    Add files via upload

---
+++ clsmsrlr7.4.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Tarih{
+	int gun,ay,yil;
+	public:
+		Tarih(int _gun =1,int _ay=1,int _yil=2012):gun(_gun),ay(_ay),yil(_yil){}
+		int arttir();
+		int goster();
+};
+
+int Tarih::arttir(){
+	gun++;
+	if(gun > aylar[ay-1]){
+		gun = 1;
+		ay++;
+	}
+	if(ay > 12){
+		ay = 1;
+		yil++;
+	}
+}
+int Tarih::goster(){
+	cout << gun << ":" << ay << ":" << yil << " ";
+}
+int main(){
+	int aylar[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+	Tarih tarih(30,7);
+	cout << "Bugun: ";
+	tarih.goster();
+	cout << "Onumuzdeki 10 Gun: ";
+	for(int i=0; i<10; i++){
+		tarih.arttir();
+		tarih.goster();
+	}
+	return 0;
+}
+
